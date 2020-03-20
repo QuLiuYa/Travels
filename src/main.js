fd40ce4cf2bf2c60f0cf5ffffae3bfd3304f4eac
chunfa <602636553@qq.com>
2020-03-21 00:07:29 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import FastClick from 'fastclick'
 import 'styles/iconfont.css'
+import 'swiper/css/swiper.css'
 
 Vue.config.productionTip = false
 // 解决 点击 300 延迟
 FastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="swiper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item in swiperUrl" :key="item.id">
+        <img class="swiperimg" :src="item.url" alt />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperUrl: [
+        {
+          id: 1,
+          url:
+            'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/ea919c3fccdad801b2b00b65eab0e8ea.jpg_r_640x290x70_ce1d70ab.jpg'
+        },
+        {
+          id: 2,
+          url:
+            'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/ca640246fde83729fb1dbb095ba0bfe1.jpg_r_640x290x70_4432d456.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.swiper >>> .swiper-pagination-bullet-active
+  background: #fff
+.swiper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 53.4%
+  .swiperimg
+    width: 100%
+</style>

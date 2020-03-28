d6d53b9b85d25746cf93a329093bae7f189e28a1
chunfa <602636553@qq.com>
2020-03-29 00:24:04 +0800

    city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style scoped lang="stylus">
+@import '~styles/varibles.styl'
+.list
+  position absolute
+  display flex
+  flex-direction column
+  justify-content center
+  width .4rem
+  top 1.58rem
+  right 0
+  bottom 0
+  .item
+    line-height .4rem
+    color $bgcolor
+    text-align center
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">你的位置</div>
+        <div class="button-list">
+          <div class="button-wrap">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrap">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrap">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrap">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrap">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <ul class="item-list">
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+        </ul>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <ul class="item-list">
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+          <li class="item border-bottom">阿里安安</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.border-topbottom
+  &:before
+    border-color #ccc
+  &:after
+    border-color #ccc
+.border-bottom
+  &:before
+    border-color #ccc
+.list
+  position absolute
+  overflow hidden
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  .title
+    line-height .44rem
+    padding-left .2rem
+    background #eee
+    color #666
+    font-size .26rem
+  .button-list
+    overflow hidden
+    padding .1rem .6rem .1rem .1rem
+    .button-wrap
+      float left
+      width 33.33%
+      .button
+        margin .1rem
+        padding .1rem 0
+        border .02rem solid #ccc
+        border-radius .06rem
+        text-align center
+  .item-list
+    .item
+      line-height .76rem
+      padding-left .2rem
+</style>
